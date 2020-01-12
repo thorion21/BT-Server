9ed7aa2dbca0d29b400c9a48863901483d63987e
thorion21 <blazegamingro@gmail.com>
2020-01-13 00:27:56 +0200

    Empty Project

---
+++ BT-Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BT_Server
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("salut");
+        }
+    }
+}
